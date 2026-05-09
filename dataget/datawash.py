ba24b8f85209b4a6628de7c2af6209f0524702c8
zzrstd <2319855430@qq.com>
2026-05-09 11:46:36 +0800

    Second

--- dataget/datawash.py
+++ dataget/datawash.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import os
 
-df = pd.read_csv(r"D:\zzr\Quant\SelfQuant\dataget\data\tu_data.csv")
+csv_path = os.path.join(os.getcwd(), "dataget/data", "tu_data.csv")
+df = pd.read_csv(csv_path)
 line_num = df.shape[0]
 df = df[df['name'].str.contains('ST|\\*ST') == False]
 df = df[df['list_date'] <= 20180101]
 line_num_after = df.shape[0]
 print(f"总行数: {line_num}, 去除ST后行数: {line_num_after}, 去除比例: {(line_num - line_num_after) / line_num:.2%}")
-df.to_csv(r"D:\zzr\Quant\SelfQuant\dataget\data\tu_data_cleaned.csv", index=False)
+df.to_csv(csv_path.replace("tu_data.csv", "tu_data_cleaned.csv"), index=False)
